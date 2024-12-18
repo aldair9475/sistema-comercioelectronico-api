54b36fd32a0f7a709743ef287e4b752b463d56ba
zairchavez17 <ac3989665@gmail.com>
2024-12-18 14:07:15 -0500

    categorias_productos service

--- src/controllers/categorias_productos.controller.ts
+++ src/controllers/categorias_productos.controller.ts
@@ -1,22 +1,34 @@
 import { Request, Response } from 'express';
+import * as categoriaProductoService from '../services/categorias_productos.service';
 
 export const insertarCategoriaProducto = async (req: Request, res: Response) => {
-    res.json({ accion: `insertarCategoriaProducto` });
-};
+    const categoriaProducto = req.body;
+    const response = categoriaProductoService.insertarCategoriaProducto(categoriaProducto);
+    res.json(response);
+}
 
 export const listarCategoriaProducto = async (req: Request, res: Response) => {
-    res.json({ accion: `listarCategoriaProducto` });
-};
+    const response = categoriaProductoService.listarCategoriaProducto();
+    res.json(response);
+}
 
 export const obtenerCategoriaProducto = async (req: Request, res: Response) => {
-    res.json({ accion: `obtenerCategoriaProducto` });
-};
+    const { idCategoriaProducto } = req.params;
+    const response = categoriaProductoService.obtenerCategoriaProducto(Number(idCategoriaProducto));
+    res.json(response);
+}
 
 export const actualizarCategoriaProducto = async (req: Request, res: Response) => {
-    res.json({ accion: `actualizarCategoriaProducto` });
-};
+    const { idCategoriaProducto } = req.params;
+    const categoriaProducto = req.body;
+    const response = categoriaProductoService.actualizarCategoriaProducto(Number(idCategoriaProducto), categoriaProducto);
+    res.json(response);
+}
 
 export const darBajaCategoriaProducto = async (req: Request, res: Response) => {
-    res.json({ accion: `darBajaCategoriaProducto` });
-};
+    const { idCategoriaProducto } = req.params;
+    const response = categoriaProductoService.darBajaCategoriaProducto(Number(idCategoriaProducto));
+    res.json(response);
+}
+
 
